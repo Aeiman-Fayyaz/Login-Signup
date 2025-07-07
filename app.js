e33739088b1cc29a9614420f0bb74f6eff592265
Aeiman-Fayyaz <Aeimanfayyaz2002@gmail.com>
2025-07-07 13:10:17 +0500

    Google Authentication

--- app.js
+++ app.js
@@ -2,18 +2,28 @@ const supabaseUrl = "https://hwyofckifjeewvmagsgc.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3eW9mY2tpZmplZXd2bWFnc2djIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0Mzk0MDcsImV4cCI6MjA2NzAxNTQwN30.pvsjYctkRI7NHdJrSuatOb-oLoatl8dIZfHUKv74gvE";
 
-const {createClient} = supabase
-const client = createClient(supabaseUrl,supabaseKey)
+const { createClient } = supabase;
+const client = createClient(supabaseUrl, supabaseKey);
 
 console.log(createClient);
 
-const loginBtn = document.getElementById("btn")
-const userEmail = document.getElementById("email")
-const userPassword = document.getElementById("password")
+const loginBtn = document.getElementById("btn");
+const userEmail = document.getElementById("email");
+const userPassword = document.getElementById("password");
 
-loginBtn.addEventListener("click" , (e) =>{
-    let userEmailVal = userEmail.value
-    console.log(userEmailVal);
-    let userPasswordVal = userPassword.value
-    console.log(userPasswordVal);
-})
+loginBtn.addEventListener("click", (e) => {
+  let userEmailVal = userEmail.value;
+  console.log(userEmailVal);
+  let userPasswordVal = userPassword.value;
+  console.log(userPasswordVal);
+});
+
+// Google Button
+const googleBtn = document.getElementById("google-btn");
+console.log(googleBtn);
+googleBtn.addEventListener("click", async (e) => {
+  const data = await client.auth.signInWithOAuth({
+    provider: 'google',
+  })
+  console.log(data);
+});
